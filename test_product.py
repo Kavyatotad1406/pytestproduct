f33d29c2c615d6c02fbb9225acc028e9bd08d95e
Kavyatotad1406 <totadkavya@gmail.com>
2025-12-01 15:31:12 +0530

    Update product ID in product details test

--- test_product.py
+++ test_product.py
@@ -3,8 +3,8 @@ from product import product_details
 def test_product_details():
     expected_output = (
         "Product Name   : Laptop\n"
-        "Product ID     : E1007\n"
+        "Product ID     : F1007\n"
         "Quantity      : \n1"
         "Price          : 95000"
     )
-    assert product_details("Laptop", "E1007", 1, 95000) == expected_output
+    assert product_details("Laptop", "F1007", 1, 95000) == expected_output
